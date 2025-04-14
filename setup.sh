2570f95a9abf81f54e6984ae0e756fefec39278b
João Laranjeira <77790402+jpclrocha@users.noreply.github.com>
2025-04-14 09:15:24 -0300

    Made some changes, now the script installs oh my zsh from git, and not from the sh version. Also added basic packages on Debian and Arch based systems, like base-devel on Arch and build-essential on Debian

--- setup.sh
+++ setup.sh
@@ -7,17 +7,30 @@ pkgs="zsh git curl tmux stow"
 if [ -f /etc/debian_version ]; then
     echo "Detected Debian-based system."
     sudo apt-get update
+    sudo apt install build-essential
     sudo apt-get -y --ignore-missing install $pkgs
 elif [ -f /etc/arch-release ]; then
     echo "Detected Arch-based system."
-    sudo pacman -Syu --noconfirm $pkgs
+    sudo pacman -Syu --noconfirm
+    sudo pacman -Syu --noconfirm $pkgs base-devel
 else
     echo "Unsupported Linux distribution."
     exit 1
 fi
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Change default shell to zsh
+printf "Changing default shell to zsh"
+chsh -s /bin/zsh
+
+# Install oh-my-zsh and some plugins
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+
 
 # Install Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
@@ -25,6 +38,9 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-
 # Install Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Delete default files created by the installations above
+rm $HOME/.zshrc
+rm $HOME/.p10k.zsh
 
 # Use stow for symlinking the dotfiles
 for dir in */; do
@@ -54,15 +70,15 @@ done
 # Docs: https://mise.jdx.dev/
 # The mise activate is already in the zshrc
 curl https://mise.run | sh
-mise use --global node@22.14.0
-mise use --global java@temurin-21.0.3+9.0.LTS
-mise use --global rust@1.85.1
-mise use --global maven@3.9.9
-mise use --global go@1.24.2
-mise use --global python@3.13.2
-mise use --global usage
+$HOME/.local/bin/mise use --global node@22.14.0
+$HOME/.local/bin/mise use --global java@temurin-21.0.3+9.0.LTS
+$HOME/.local/bin/mise use --global rust@1.85.1
+$HOME/.local/bin/mise use --global maven@3.9.9
+$HOME/.local/bin/mise use --global go@1.24.2
+$HOME/.local/bin/mise use --global python@3.13.2
+$HOME/.local/bin/mise use --global usage
 
 # Rewritten in Rust: Modern Alternatives of Command-Line Tools
 # https://zaiste.net/posts/shell-commands-rust/
 # Yazi for browsing files in the terminal
-cargo install bat du-dust eza fd-find ripgrep yazi-cli yazi-fm
+/home/jope/.cargo/bin/cargo install bat du-dust eza fd-find ripgrep yazi-cli yazi-fm
